9ade5cd8d2b555b126a26b3b4b7e52070d0345df
khushikumarigupta14 <khushikumarigupta463@gmail.com>
2025-03-21 11:10:02 +0530

    Add User model for user data management

---
+++ backend/src/models/UserModel.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+  isAdmin: { type: Boolean, default: false },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
